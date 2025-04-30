6749f78b222a01bf67bf226991a8c82e1134150a
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-04-30 22:41:40 +0300

    фиксы

--- accounts/templatetags/accounts_extras.py
+++ accounts/templatetags/accounts_extras.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.safestring import mark_safe
+import json
 
 register = template.Library()
 
@@ -30,3 +32,44 @@ def translate_category(name):
 def multiply(qty, unit_price, *args, **kwargs):
     # example usage in template: {% multiply value1 value2 %}
     return qty * unit_price 
+
+@register.filter(name='get_file_url')
+def get_file_url(file_id, entity_id, file_type):
+    # Ваша логика получения URL файла
+    # Например:
+    # file = FileStorage.objects.filter(file_id=file_id, entity_id=entity_id, file_type=file_type).first()
+    # if file:
+    #     return file.get_absolute_url() # Или другой метод получения URL
+    # return '#' # Возвращаем заглушку или обрабатываем ошибку
+    # !!! Замените этот комментарий на реальную логику !!!
+    # Пока что возвращаем примерный путь для локальной разработки или заглушку
+    # return f'/media/{file_type}/{entity_id}/{file_id}'
+    return '#' # Заглушка
+
+@register.filter(name='jsonify')
+def jsonify(data):
+    return mark_safe(json.dumps(data))
+
+@register.filter(name='startswith')
+def startswith(text, starts):
+    if isinstance(text, str):
+        return text.startswith(starts)
+    return False
+
+@register.filter(name='has_invested')
+def has_invested(user, startup):
+    # Заглушка - замените на реальную проверку инвестиций пользователя в стартап
+    # return Investment.objects.filter(user=user, startup=startup).exists()
+    return False # Пример
+
+@register.filter(name='is_buyout_investor')
+def is_buyout_investor(user, startup):
+    # Заглушка - замените на реальную проверку, является ли пользователь инвестором типа "Выкуп"
+    # return Investment.objects.filter(user=user, startup=startup, investment_type='buyout').exists()
+    return True # Пример
+
+# Добавляем фильтр get_item
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    """Позволяет получить значение из словаря по ключу в шаблоне Django."""
+    return dictionary.get(key) 
