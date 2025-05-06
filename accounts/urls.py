0c4e591aaf4aeab80ae7e4d32ba1d1ffd4d6e4ce
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-05-06 17:50:02 +0300

    фикс

--- accounts/urls.py
+++ accounts/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('my_startups/', views.my_startups, name='my_startups'),  # Изменяем my-startups на my_startups
     # URL для подгрузки похожих стартапов - ИСПРАВЛЯЕМ ТИП ID НА INT
     path('load_similar_startups/<int:startup_id>/', views.load_similar_startups, name='load_similar_startups'),
-    path('startup/<int:startup_id>/vote/<str:vote_type>/', views.submit_vote, name='submit_vote'),
+    path('startup/<int:startup_id>/vote/<str:vote_type>/', views.vote_startup, name='submit_vote'),
     path('startup/<int:startup_id>/add_comment/', views.add_comment, name='add_comment'),
     path('startup/<int:startup_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('upload_file/', views.upload_file, name='upload_file'),
