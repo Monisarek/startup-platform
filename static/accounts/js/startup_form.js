b72026a4e344e922cc785fc097661ed1c529e036
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-05-16 15:48:03 +0300

    Create startup_form.js

---
+++ static/accounts/js/startup_form.js
@@ -0,0 +1,5 @@
+// Javascript for startup_form.html
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('startup_form.js loaded');
+    // TODO: Add specific logic for edit startup form
+}); 
