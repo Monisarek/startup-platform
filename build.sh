0ee283bdf1a96e2ff400a076e1853768c708df2d
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-06-09 19:15:53 +0300

    Chore(build): Add step to print Vite manifest

--- build.sh
+++ build.sh
@@ -11,8 +11,12 @@ npm install
 echo "--- Building frontend ---"
 npm run build
 
+echo "--- VITE MANIFEST CONTENT ---"
+cat static/dist/.vite/manifest.json || echo "Manifest file not found"
+echo "--- END VITE MANIFEST CONTENT ---"
+
 echo "--- Collecting static files ---"
 # Используем Python из виртуального окружения проекта напрямую
-/opt/render/project/src/.venv/bin/python manage.py collectstatic --noinput
+/opt/render/project/src/.venv/bin/python manage.py collectstatic --noinput --clear
 
 echo "--- Build successful ---" 
