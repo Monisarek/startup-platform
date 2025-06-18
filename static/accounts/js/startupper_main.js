8d629030d41377ec35bb152432790a3d72ad44f3
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-06-18 12:32:16 +0300

    вкид без деплоя

---
+++ static/accounts/js/startupper_main.js
@@ -0,0 +1,6 @@
+// This is a placeholder for JavaScript code for the main_temp page.
+// We can add functionality here later if needed.
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Блок скрипта для sticky эффекта был здесь и теперь удален.
+})
