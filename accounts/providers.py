feea8124f3b26e2e6666e057ddb0a2379091dd2c
Maxim <88581061+Monisarek@users.noreply.github.com>
2025-06-20 16:26:00 +0300

    в2

--- accounts/providers.py
+++ accounts/providers.py
@@ -1,13 +1,24 @@
-from allauth.socialaccount.adapter import OAuth2Adapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.socialaccount.providers.telegram.provider import TelegramProvider
+import requests
 
-class CustomTelegramAdapter(OAuth2Adapter):
+class CustomTelegramAdapter(DefaultSocialAccountAdapter):
     provider_id = TelegramProvider.id
-    access_token_url = 'https://api.telegram.org/bot{}/getMe'.format(TelegramProvider.id)
-    authorize_url = 'https://oauth.telegram.org/auth'
-    profile_url = 'https://api.telegram.org/bot{}/getUserProfilePhotos'.format(TelegramProvider.id)
 
     def complete_login(self, request, app, token, **kwargs):
-        # Минимальная реализация для совместимости с 0.61.1
-        from allauth.socialaccount.helpers import complete_social_login
-        return complete_social_login(request, app)
+        # Получаем данные пользователя через Telegram API
+        api_url = f'https://api.telegram.org/bot{app.client_id}/getMe'
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            # Простая обработка данных (нужно адаптировать под вашу модель Users)
+            user_data = {
+                'id': data.get('result', {}).get('id'),
+                'username': data.get('result', {}).get('username'),
+                'first_name': data.get('result', {}).get('first_name'),
+                'last_name': data.get('result', {}).get('last_name'),
+            }
+            from allauth.socialaccount.helpers import complete_social_login
+            return complete_social_login(request, app, user_data)
+        else:
+            raise Exception("Failed to fetch user data from Telegram API")
