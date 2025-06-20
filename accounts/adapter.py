18e2a96af17fe4af55dc2b9669907afd29b7a27e
Maxim <88581061+Monisarek@users.noreply.github.com>
2025-06-20 19:10:47 +0300

    бекап

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -1,35 +1,18 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
-from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
 
-    def build_provider_url(self, request, provider, return_url):
+    def get_connect_redirect_url(self, request, socialaccount):
         """
-        Перехватывает создание URL для входа и принудительно добавляет 'www',
-        исправляя параметры 'origin' и 'return_to' для Telegram.
+        Переопределяем метод, чтобы принудительно использовать WWW для домена.
+        Это гарантирует, что Telegram получит правильный `redirect_uri`.
         """
-        # Сначала получаем стандартный URL, который сгенерировал бы allauth
-        url = super().build_provider_url(request, provider, return_url)
+        # Получаем стандартный URL, который сгенерировал бы allauth
+        url = super().get_connect_redirect_url(request, socialaccount)
 
-        # В продакшене (когда DEBUG=False) ищем наш домен
-        if not settings.DEBUG and 'greatideas.ru' in url:
-            
-            parsed_url = urlparse(url)
-            query_params = parse_qs(parsed_url.query)
-
-            # Проверяем и модифицируем параметры 'origin' и 'return_to'
-            for param_name in ['origin', 'return_to']:
-                if param_name in query_params and query_params[param_name]:
-                    inner_url_str = query_params[param_name][0]
-                    if '://greatideas.ru' in inner_url_str:
-                        new_inner_url = inner_url_str.replace('://greatideas.ru', '://www.greatideas.ru')
-                        query_params[param_name][0] = new_inner_url
-            
-            # Собираем URL обратно с исправленными параметрами
-            new_query_string = urlencode(query_params, doseq=True)
-            url = urlunparse(
-                (parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, new_query_string, parsed_url.fragment)
-            )
+        # Если мы в продакшене и URL содержит наш домен без 'www', исправляем его
+        if not settings.DEBUG and '://greatideas.ru' in url:
+            url = url.replace('://greatideas.ru', '://www.greatideas.ru')
             
         return url 
