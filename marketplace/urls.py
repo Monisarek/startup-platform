2e59f93cd5a2ae9911dc0a0a5dca8269fc12268e
Maxim <88581061+Monisarek@users.noreply.github.com>
2025-06-21 18:40:30 +0300

    Update urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 urlpatterns = [
-    path('login/', include('accounts.urls')),  # Для твоего /login/
     path("", include("accounts.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Полный путь для allauth
