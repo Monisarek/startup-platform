12c59d3d43e53f702a9e66e400613179c19a21d1
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-06 19:14:59 +0300

    Fix: добавлены недостающие поля в таблицу franchises

--- accounts/migrations/0025_add_franchises_model.py
+++ accounts/migrations/0025_add_franchises_model.py
@@ -1,6 +1,7 @@
 # Generated manually
 
-from django.db import migrations
+from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -10,5 +11,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # Пустая миграция - таблица franchises уже существует в базе данных
+        migrations.AddField(
+            model_name='franchises',
+            name='investment_size',
+            field=models.DecimalField(blank=True, decimal_places=4, max_digits=19, null=True),
+        ),
+        migrations.AddField(
+            model_name='franchises',
+            name='payback_period',
+            field=models.IntegerField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='franchises',
+            name='own_businesses',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='franchises',
+            name='franchise_businesses',
+            field=models.IntegerField(default=0),
+        ),
     ] 
