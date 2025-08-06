21fe992dbaba4ef0cc79841ff5176a860e323396
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-06 10:39:52 +0300

    Размещаем планетарку в правой части первого блока - увеличили масштаб до 1.4 и правильно позиционировали сучку

--- static/accounts/js/planetary_system.js
+++ static/accounts/js/planetary_system.js
@@ -67,7 +67,7 @@
     if (currentPage === 'home') {
       ultraNewPlanetaryGalaxyY = 0;
       ultraNewPlanetaryGalaxyX = 0;
-      ultraNewPlanetaryGalaxyScale = 1.2;
+      ultraNewPlanetaryGalaxyScale = 1.4;
     } else if (currentPage === 'main') {
       ultraNewPlanetaryGalaxyY = -200;
       ultraNewPlanetaryGalaxyScale = 0.8;
