a78cacc53980972b9e2116739cf3274336dee52d
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-06 19:10:42 +0300

    Fix: создана пустая миграция для модели Franchises

---
+++ accounts/migrations/0025_add_franchises_model.py
@@ -0,0 +1,14 @@
+# Generated manually
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0024_add_vk_linkedin_to_users_only'),
+    ]
+
+    operations = [
+        # Пустая миграция - таблица franchises уже существует в базе данных
+    ] 
