0bdec8702fb903a4eaa936e7d289ac090e171940
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-10 19:49:27 +0300

    merge: разрешен конфликт миграций accounts (0040_...)

---
+++ accounts/migrations/0041_merge_seed_specialists.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0040_franchisecategories_agencies_direction_and_more"),
+        ("accounts", "0040_seed_specialists"),
+    ]
+
+    operations = []
+
+
