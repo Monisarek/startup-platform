48a64b8e9d97c6e3863383d6b2badd02c18d888d
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-10 20:05:12 +0300

    merge: объединение листьев миграций accounts (0042, 0043)

---
+++ accounts/migrations/0044_merge_specialists_leaves.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0042_merge_specialists_branch"),
+        ("accounts", "0043_add_specialists_pk_column"),
+    ]
+
+    operations = []
+
+
