238cb03710eff7f0f52c8e880b5fda12bd250ff5
Даниил <163737717+Dauuy@users.noreply.github.com>
2025-08-15 12:07:36 +0300

    merge миграций 0046

---
+++ accounts/migrations/0047_merge_fix_agency_and_specialist.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0046_fix_agencies_pk_default"),
+        ("accounts", "0046_update_specialist_categories"),
+    ]
+
+    operations = []
+
+
